f877502e4a350a931704e659abff4ca1399f2b11
kimdimka <kimdimka@yandex.ru>
2022-04-07 21:20:56 +1000

    добавлена синхронизация потоков, и чтение параметров из апп конфига

--- RaytecPreactorFileSyncPrj/Service1.cs
+++ RaytecPreactorFileSyncPrj/Service1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
@@ -76,26 +77,50 @@ namespace RaytecPreactorFileSyncPrj
         bool enabled = true;
         int tryCount = 0;
         bool notCopied = false;
+        bool notCopiedDll = false;
+        string sourcePath;
+        string filter;
+        string sourcePathDll;
+        string filterDll;
+        string destinationPath;
+        string destinationPathDll;
+        int timeInterval_ms;
+
+        string sourceFullPathChanged;
+        string sourceFullPathDllChanged;
+        string sourceFileNameChanged;
+        string sourceFileNameDllChanged;
+        object lockFileRes = new object();
+        object lockFileDllRes = new object();
+
         public PreactorFileObserver()
         {
-            string path = "c:\\1.txt";
+            sourcePath = ConfigurationManager.AppSettings.Get("SourcePath");
+            filter = ConfigurationManager.AppSettings.Get("Filter"); 
+            sourcePathDll = ConfigurationManager.AppSettings.Get("SourcePathDll");
+            filterDll = ConfigurationManager.AppSettings.Get("FilterDll");
+            destinationPath= ConfigurationManager.AppSettings.Get("DestinationPath"); 
+            destinationPathDll = ConfigurationManager.AppSettings.Get("DestinationPathDll");
+
+            bool isParseSuccess = int.TryParse(ConfigurationManager.AppSettings.Get("TimeInterval_ms"), out timeInterval_ms);
+            if (!isParseSuccess) timeInterval_ms = 1000;
+
+            //string path = "c:\\1.txt";
             watcher = new FileSystemWatcher();
-            string dirName = Path.GetDirectoryName(path);
-            string fileName = Path.GetFileName(path);
-            watcher.Path = dirName;
-            watcher.Filter = fileName;
+            //string dirName = Path.GetDirectoryName(path);
+            //string fileName = Path.GetFileName(path);
+            watcher.Path = sourcePath;
+            watcher.Filter = filter;
             watcher.Changed += FileChanged;
 
 
-            string pathDll = "d:\\2.txt";
+            //string pathDll = "d:\\2.txt";
             watcherDll = new FileSystemWatcher();
-            string dirNameDll = Path.GetDirectoryName(pathDll);
-            string fileNameDll = Path.GetFileName(pathDll);
-            watcherDll.Path = dirNameDll;
-            watcherDll.Filter = fileNameDll;
+            //string dirNameDll = Path.GetDirectoryName(pathDll);
+            //string fileNameDll = Path.GetFileName(pathDll);
+            watcherDll.Path = sourcePathDll;
+            watcherDll.Filter = filterDll;
             watcherDll.Changed += FileChangedDll;
-
-
         }//PreactorFileObserver()
         private string GetMD5HAsh(string path)
         {
@@ -115,7 +140,7 @@ namespace RaytecPreactorFileSyncPrj
 
             try
             {
-                fInfo.CopyTo("c:\\zzz\\" + fileName, true);
+                fInfo.CopyTo(destinationPath + fileName, true);
                 notCopied = false;
             }
             catch (Exception ex)
@@ -125,14 +150,44 @@ namespace RaytecPreactorFileSyncPrj
             }
         }//CopyFiles()
 
+        private void CopyFilesDll(string fullPath, string fileName)
+        {
+            FileInfo fInfo = new FileInfo(fullPath);
+
+            try
+            {
+                fInfo.CopyTo(destinationPath + fileName, true);
+                notCopiedDll = false;
+            }
+            catch (Exception ex)
+            {
+                notCopiedDll = true;
+
+            }
+        }//CopyFiles()
+
         private bool CompareFile(string fullPath, string fileName)
         {
-            return GetMD5HAsh(fullPath) == GetMD5HAsh("c:\\zzz\\" + fileName);
+            DirectoryInfo dirInfo = new DirectoryInfo(destinationPath);
+            FileInfo fInfo = new FileInfo(fullPath);
+            if (!dirInfo.Exists)
+                return true;
+            if (!fInfo.Exists)
+                return true;
+
+            return GetMD5HAsh(fullPath) == GetMD5HAsh(destinationPath + fileName);
         }//CompareFile()
 
         private bool CompareFileDll(string fullPath, string fileName)
         {
-            return GetMD5HAsh(fullPath) == GetMD5HAsh("c:\\zzz\\" + fileName);
+            DirectoryInfo dirInfo = new DirectoryInfo(destinationPath);
+            FileInfo fInfo = new FileInfo(fullPath);
+            if (!dirInfo.Exists)
+                return true;
+            if (!fInfo.Exists)
+                return true;
+
+            return GetMD5HAsh(fullPath) == GetMD5HAsh(destinationPath + fileName);
         }//CompareFileDll()
 
 
@@ -147,6 +202,11 @@ namespace RaytecPreactorFileSyncPrj
             string fullPath = e.FullPath;
             string fileName = e.Name;
             CopyFiles(fullPath, fileName);
+            lock (lockFileRes)
+            {
+                sourceFullPathChanged = fullPath;
+                sourceFileNameChanged = fileName;
+            }
 
         }//FileChanged()
 
@@ -160,26 +220,43 @@ namespace RaytecPreactorFileSyncPrj
             #endregion
             string fullPath = e.FullPath;
             string fileName = e.Name;
-            CopyFiles(fullPath, fileName);
+            CopyFilesDll(fullPath, fileName);
+            lock (lockFileDllRes)
+            {
+                sourceFullPathDllChanged = fullPath;
+                sourceFileNameDllChanged = fileName;
+            }
 
         }//FileChangedDll()
 
         public void Start()
         {
+            string fullPath;
+            string fileName;
+            string fullPathDll;
+            string fileNameDll;
+
             watcher.EnableRaisingEvents = true;
             watcherDll.EnableRaisingEvents = true;
             while (enabled)
             {
-                Thread.Sleep(1000);
-                string fullPath = "c:\\1.txt";
-                string fileName = "1.txt";
-                string fullPathDll = "d:\\2.txt";
-                string fileNameDll = "2.txt";
-                notCopied = !CompareFile(fullPath, fileName) || !CompareFileDll(fullPathDll, fileNameDll);
-                if (notCopied==true)
+                fullPath = sourceFullPathChanged;
+                fileName = sourceFileNameChanged;
+                fullPathDll = sourceFullPathDllChanged;
+                fileNameDll = sourceFileNameDllChanged;
+
+                bool retry = notCopied == true ? !CompareFile(fullPath, fileName): notCopied;
+
+                if (retry == true)
                 {
                     CopyFiles(fullPath, fileName);
                 }//if
+                retry = notCopiedDll == true ? !CompareFileDll(fullPathDll, fileNameDll): notCopiedDll;
+                if (retry == true)
+                {
+                    CopyFilesDll(fullPathDll, fileNameDll);
+                }//if
+                Thread.Sleep(timeInterval_ms);
 
             }//while
 
